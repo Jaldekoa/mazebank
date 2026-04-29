f2955bf6d5e2c68949890711560c5605052aaadf
Jon Aldekoa <jaldekoa@gmail.com>
2026-04-29 23:26:41 +0200

    Rename constants to fit new .env variables

--- src/config/db.js
+++ src/config/db.js
@@ -1,25 +1,39 @@
 import { Sequelize } from "sequelize";
 import { logerror } from "../utils/utils.js";
 
-const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
+const {
+  POSTGRES_NAME,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_HOST,
+  POSTGRES_PORT,
+} = process.env;
 
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
-  port: DB_PORT,
-  dialect: "postgres",
-});
+const sequelize = new Sequelize(
+  POSTGRES_NAME,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  {
+    host: POSTGRES_HOST,
+    port: POSTGRES_PORT,
+    dialect: "postgres",
+  },
+);
 
 async function checkDB() {
   try {
     await sequelize.authenticate();
   } catch (error) {
-    logerror.red("❌ An error has occurred while connecting to the database: ", error);
+    logerror.red(
+      "❌ An error has occurred while connecting to the database: ",
+      error,
+    );
   }
 }
 
 async function syncDB() {
   try {
-    await sequelize.sync({ alter: true });
+    await sequelize.sync();
   } catch (error) {
     logerror.red("❌ Synchronization failed: ", error);
   }
