389258546c71e65d703b0d2a6451824923174523
Jon Aldekoa <jaldekoa@gmail.com>
2026-04-29 23:31:35 +0200

    Add createTransaction controller

--- src/controllers/api/transaction.controller.js
+++ src/controllers/api/transaction.controller.js
@@ -1,11 +1,24 @@
 import transactionServices from "../../services/transaction.service.js";
 
+async function createTransaction(req, res) {
+  try {
+    const { senderAccountId, receiverAccountId, amount, details } = req.body;
+  
+    const transaction = await transactionServices.createTransaction(senderAccountId, receiverAccountId, amount, details);
+    return res.status(201).json(transaction);
+  
+  } catch (error) {
+    
+    return res.status(400).json(error.message);
+  };
+}
+
 async function getAllTransactionsForAccount(req, res) {
   const data = await transactionServices.getAllTransactionsForAccount(
     req.params.userId,
   );
-  return res.json(data);
+  return res.status(200).json(data);
 }
 
-export const transactionController = { getAllTransactionsForAccount };
+export const transactionController = { getAllTransactionsForAccount, createTransaction };
 export default transactionController;
