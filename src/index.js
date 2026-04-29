f531742e8be72fac417c862e16cd97386891bd4a
Jon Aldekoa <jaldekoa@gmail.com>
2026-04-29 02:23:40 +0200

    Add start connection to DB

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import express from "express";
 import { checkDB, syncDB } from "./config/db.js";
+import { log } from "./utils/utils.js";
+import router from "./routes/routes.js";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use("/", router);
 
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
@@ -15,6 +19,9 @@ app.get("/error", (req, res) => {
   res.status(400).json({ status: "error" });
 });
 
+checkDB();
+syncDB();
+
 app.listen(PORT, () => {
-  console.log(`Server successfully connected via port ${PORT}`);
+  log.green(`✔ Server successfully connected via port ${PORT}`);
 });
