9e2d91c0fb4899be54d3682a3b9003a7c26d802a
Jon Aldekoa <jaldekoa@gmail.com>
2026-04-29 23:30:33 +0200

    Create template for Account and User controller

---
+++ src/controllers/api/account.controller.js
@@ -0,0 +1,4 @@
+import accountServices from "../../services/account.service.js";
+
+export const accountController = {};
+export default accountController;
---
+++ src/controllers/api/user.controller.js
@@ -0,0 +1,4 @@
+import userServices from "../../services/user.service.js";
+
+export const userController = {};
+export default userController;
