e124fd42187412df95e253d1a236da85e039c708
Jon Aldekoa <jaldekoa@gmail.com>
2026-04-29 23:31:09 +0200

    Add createTransaction and change getAllTransactionsForAccount

--- src/services/transaction.service.js
+++ src/services/transaction.service.js
@@ -1,13 +1,39 @@
-import { TransactionsModel } from "../models/index.js";
+import sequelize from "../config/db.js";
+import { AccountModel, TransactionsModel } from "../models/index.js";
 import { log } from "../utils/utils.js";
+import { Op } from "sequelize";
+
+async function createTransaction(senderAccountId, receiverAccountId, amount, details) {
+  const transaction = await sequelize.transaction();
+
+  try {
+    const senderAccount = await AccountModel.findByPk(senderAccountId, {transaction: transaction, lock: true});
+    const receiverAccount = await AccountModel.findByPk(receiverAccountId, {transaction: transaction, lock: true});
+  
+    await senderAccount.update({ balance: Number(senderAccount.balance) - Number(amount) });
+    await receiverAccount.update({ balance: Number(receiverAccount.balance) + Number(amount) });
+  
+    const newTransaction = await TransactionsModel.create({ senderAccountId, receiverAccountId, amount, details, type: 'TRANSFER' }, { transaction: transaction });
+    await transaction.commit();
+
+    return newTransaction;
+  } catch (error) {
+    await transaction.rollback();
+  };
+
+};
 
 async function getAllTransactionsForAccount(accountId) {
-  log.red(Object.keys(accountId));
   const transactions = await TransactionsModel.findAll({
-    where: { accountId: accountId },
+    where: {
+      [Op.or]: [
+        { senderAccountId: accountId },
+        { receiverAccountId: accountId },
+      ],
+    },
   });
   return transactions;
 }
 
-export const transactionServices = { getAllTransactionsForAccount };
+export const transactionServices = { getAllTransactionsForAccount, createTransaction };
 export default transactionServices;
