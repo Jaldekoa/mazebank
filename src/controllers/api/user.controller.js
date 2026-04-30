40fe61a2578788ae27105c4d0e256bcfba6bfc17
Jon Aldekoa <jaldekoa@gmail.com>
2026-04-30 13:54:55 +0200

    Create registerUser controller

--- src/controllers/api/user.controller.js
+++ src/controllers/api/user.controller.js
@@ -1,4 +1,15 @@
 import userServices from "../../services/user.service.js";
 
-export const userController = {};
+const registerUser = async (req, res) => {
+  try {
+    const { dni, password, firstName, lastName, email, phoneNumber } = req.body;
+    const newUser = await userServices.registerUser(dni, password, firstName, lastName, email, phoneNumber);
+    res.status(201).json();
+
+  } catch (error) {
+    res.status(500).json();
+  }
+};
+
+export const userController = { registerUser };
 export default userController;
