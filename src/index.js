b2e9e1b69bec03cf7f386db6ae8934576334042a
Jon Aldekoa <jaldekoa@gmail.com>
2026-04-30 13:51:07 +0200

    Add CORS to backend

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import express from "express";
 import { checkDB, syncDB } from "./config/db.js";
-import { log } from "./utils/utils.js";
 import router from "./routes/routes.js";
+import { log } from "./utils/utils.js";
+import express from "express";
+import cors from "cors";
+
+const corsOptions = {
+  origin: ["https://mazebank.com", "http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/", router);
 
