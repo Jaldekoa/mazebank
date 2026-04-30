3c00edeefbee966513ecca7b3c2f90c97cecb308
Jon Aldekoa <jaldekoa@gmail.com>
2026-04-30 13:51:56 +0200

    Create account routes

--- src/routes/api/account.routes.js
+++ src/routes/api/account.routes.js
@@ -1,5 +1,12 @@
+import accountController from "../../controllers/api/account.controller.js";
 import { Router } from "express";
 
 const accountRouter = Router();
+
+accountRouter.get("/", accountController.getAllUserAccounts);
+accountRouter.post("/", accountController.createNewAccount);
+accountRouter.get("/:id", accountController.getAccountInfo);
+accountRouter.patch("/:id", accountController.changeAccountInfo);
+accountRouter.dekete("/:id", accountController.deleteAccount);
 
 export default accountRouter;
