a0ae1d89ef9f0f9a11e04844c180f07bad37c36c
Jon Aldekoa <jaldekoa@gmail.com>
2026-04-30 13:51:26 +0200

    Create auth route

--- src/routes/api/api.routes.js
+++ src/routes/api/api.routes.js
@@ -4,8 +4,8 @@ import accountRouter from "./account.routes.js";
 import transactionRouter from "./transaction.routes.js";
 
 const apiRouter = Router();
-
 apiRouter.use("/user", userRouter);
+apiRouter.post("/auth", userRouter);
 apiRouter.use("/account", accountRouter);
 apiRouter.use("/transaction", transactionRouter);
 
