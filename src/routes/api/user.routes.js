2ca50fa519391dedfc93ab5ed0249fa07dcc1f4b
Jon Aldekoa <jaldekoa@gmail.com>
2026-04-30 13:51:43 +0200

    Create user routes

--- src/routes/api/user.routes.js
+++ src/routes/api/user.routes.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
+import userController from "../../controllers/api/user.controller.js";
 
 const userRouter = Router();
+
+userRouter.get("/register", userController.register);
+userRouter.post("/login", userController.login);
+
+userRouter.get("/me", userController.getMyUserInfo);
+userRouter.patch("/me", userController.changeMyUserInfo);
 
 export default userRouter;
