453fc549b8d0e61004578536b8016c0610d37c6c
Jon Aldekoa <jaldekoa@gmail.com>
2026-04-30 13:54:02 +0200

    Create registerUser service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,4 +1,15 @@
 import { UserModel } from "../models/index.js";
+import bcrypt from "bcrypt";
 
-export const userServices = {};
+async function registerUser(dni, password, firstName, lastName, email, phoneNumber) {
+    const existUser = await User.findOne({ where: { dni } });
+    if (existUser) { return res.status(409).json() };
+    
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({ dni, password: hashedPassword, firstName, lastName, email, phoneNumber });
+
+    return newUser
+};
+
+export const userServices = { registerUser };
 export default userServices;
