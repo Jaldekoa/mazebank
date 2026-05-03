1b7a7cdc47e0069fbbc52e10412a879ab6ae95fa
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-03 22:07:06 +0200

    Add logic

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,15 +1,54 @@
 import { UserModel } from "../models/index.js";
+import jwt from 'jsonwebtoken';
 import bcrypt from "bcrypt";
 
-async function registerUser(dni, password, firstName, lastName, email, phoneNumber) {
-    const existUser = await User.findOne({ where: { dni } });
-    if (existUser) { return res.status(409).json() };
+async function registerUser(userData) {
+    const { dni, password } = userData;
     
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = await User.create({ dni, password: hashedPassword, firstName, lastName, email, phoneNumber });
+    const existUser = await UserModel.scope("withPassword").findOne({ where: { dni } });
+    if (existUser) {
+        const error = new Error("Invalid credentials");
+        error.statusCode = 409;
+        throw error;
+    };
 
-    return newUser
+    const hashedPassword = await bcrypt.hash(password, 10);
+    return await UserModel.create({ ...userData, password: hashedPassword });
 };
 
-export const userServices = { registerUser };
+async function loginUser(dni, password) {
+    const user = await UserModel.scope("withPassword").findOne({ where: { dni } });
+    if (!user) throw new Error("Invalid credentials");
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) throw new Error("Invalid credentials");
+
+    const token = jwt.sign(
+        { id: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' }
+    );
+
+    const userSafe = user.toJSON();
+    delete userSafe.password;
+
+    return { userSafe, token };
+};
+
+async function getUserByDni(dni) {
+    const user = await UserModel.findOne({ where: { dni } });
+    return user;
+};
+
+async function updateUserInfo(dni, updateData) {
+    const user = await getUserByDni(dni);
+    if (!user) throw new Error("Invalid data");
+    return await user.update(updateData);
+};
+
+async function deleteUser(dni) {
+    const user = await getUserByDni(dni);
+    if (!user) throw new Error("Invalid user");
+    await user.destroy();
+};
+
+export const userServices = { registerUser, loginUser, getUserByDni, updateUserInfo, deleteUser };
 export default userServices;
