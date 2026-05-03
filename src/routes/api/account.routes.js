2f23306bec65e696a051395639aa9cdc783d7ffa
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-03 22:05:42 +0200

    Add middleware and rename params from id to accountNumber

--- src/routes/api/account.routes.js
+++ src/routes/api/account.routes.js
@@ -1,12 +1,14 @@
 import accountController from "../../controllers/api/account.controller.js";
+import authMiddleware from "../../middlewares/auth.middleware.js";
 import { Router } from "express";
 
 const accountRouter = Router();
+accountRouter.use(authMiddleware);
 
 accountRouter.get("/", accountController.getAllUserAccounts);
 accountRouter.post("/", accountController.createNewAccount);
-accountRouter.get("/:id", accountController.getAccountInfo);
-accountRouter.patch("/:id", accountController.changeAccountInfo);
-accountRouter.delete("/:id", accountController.deleteAccount);
+accountRouter.get("/:accountNumber", accountController.getAccountInfo);
+accountRouter.patch("/:accountNumber", accountController.changeAccountInfo);
+accountRouter.delete("/:accountNumber", accountController.deleteAccount);
 
 export default accountRouter;
