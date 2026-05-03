9e5318faf0276192a2ac46f9515f2ca18f21ae67
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-03 22:06:09 +0200

    Add middleware

--- src/routes/api/user.routes.js
+++ src/routes/api/user.routes.js
@@ -1,13 +1,14 @@
-import { Router } from "express";
 import userController from "../../controllers/api/user.controller.js";
+import authMiddleware from "../../middlewares/auth.middleware.js";
+import { Router } from "express";
 
 const userRouter = Router();
 
-userRouter.get("/register", userController.register);
-userRouter.post("/login", userController.login);
+userRouter.post("/register", userController.registerUser);
+userRouter.post("/login", userController.loginUser);
 
-userRouter.get("/me", userController.getMyUserInfo);
-userRouter.patch("/me", userController.changeMyUserInfo);
-userRouter.delete("/me", userController.deleteMyUser);
+userRouter.get("/me", authMiddleware, userController.getMyUserInfo);
+userRouter.patch("/me", authMiddleware, userController.changeMyUserInfo);
+userRouter.delete("/me", authMiddleware, userController.deleteMyUser);
 
 export default userRouter;
