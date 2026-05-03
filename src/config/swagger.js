bdf226a0e1839b19aba10bce56450b7c7ac68245
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-04 01:32:22 +0200

    Add swagger specs

---
+++ src/config/swagger.js
@@ -0,0 +1,33 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Mazebank",
+      version: "1.0.0",
+      description: "Documentación interactiva de la API de Mazebank",
+    },
+    servers: [
+      {
+        url: `http://localhost:${process.env.PORT || 3000}`,
+        description: "Desarrollo",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "Bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [{ bearerAuth: [] }],
+  },
+  apis: ["./src/routes/*.js", "./src/routes/api/*.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+export { swaggerUi, swaggerSpec };
