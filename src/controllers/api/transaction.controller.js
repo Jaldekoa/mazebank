d3a8863224b2a33c400732bc82446d83211e55c5
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-03 10:53:03 +0200

    Add makeDeposit and makeWithdraw controller functions

--- src/controllers/api/transaction.controller.js
+++ src/controllers/api/transaction.controller.js
@@ -1,24 +1,42 @@
 import transactionServices from "../../services/transaction.service.js";
 
-async function createTransaction(req, res) {
+async function makeDeposit(req, res) { 
   try {
-    const { senderAccountId, receiverAccountId, amount, details } = req.body;
-  
-    const transaction = await transactionServices.createTransaction(senderAccountId, receiverAccountId, amount, details);
+    const { receiverAccountId, amount, details } = req.body;
+    const transaction = await transactionServices.createTransaction(null, receiverAccountId, amount, details);
     return res.status(201).json(transaction);
-  
+
   } catch (error) {
-    
     return res.status(400).json();
   };
-}
+};
+
+async function makeWithdraw(req, res) {
+  try {
+    const { senderAccountId, amount, details } = req.body;
+    const transaction = await transactionServices.createTransaction(senderAccountId, null, amount, details);
+    return res.status(201).json(transaction);
+
+  } catch (error) {
+    return res.status(400).json();
+  };
+ };
+
+async function makeTransfer(req, res) { 
+  try {
+    const { senderAccountId, receiverAccountId, amount, details } = req.body;
+    const transaction = await transactionServices.createTransaction(senderAccountId, receiverAccountId, amount, details);
+    return res.status(201).json(transaction);
+
+  } catch (error) {
+    return res.status(400).json();
+  };
+};
 
 async function getAllTransactionsForAccount(req, res) {
-  const data = await transactionServices.getAllTransactionsForAccount(
-    req.params.userId,
-  );
+  const data = await transactionServices.getAllTransactionsForAccount(req.params.userId);
   return res.status(200).json(data);
 }
 
-export const transactionController = { getAllTransactionsForAccount, createTransaction };
+export const transactionController = { getAllTransactionsForAccount, makeDeposit, makeWithdraw, makeTransfer };
 export default transactionController;
