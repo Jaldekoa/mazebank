6b61fd986707e0fe3580a929da6eae1a61d63329
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-03 22:08:07 +0200

    Add logic for account.service.js

--- src/services/account.service.js
+++ src/services/account.service.js
@@ -1,13 +1,35 @@
-import sequelize from "../config/db.js";
 import { AccountModel } from "../models/index.js";
-import { log } from "../utils/utils.js";
-import { Op } from "sequelize";
 
-async function getAllUserAccounts() {};
-async function createNewAccount() {};
-async function getAccountInfo() {};
-async function changeAccountInfo() {};
-async function deleteAccount() {};
+async function getAllUserAccounts(userId) {
+    return await AccountModel.findAll({ where: { userId } });
+}
 
-export const accountServices = {getAllUserAccounts, createNewAccount, getAccountInfo, changeAccountInfo, deleteAccount };
-export default accountServices;
+async function createNewAccount(userId, accountData) {
+    const { accountName } = accountData;
+    return await AccountModel.create({ accountName, userId });
+}
+
+async function getAccountInfo(userId, accountNumber) {
+    const account = await AccountModel.findOne({ where: { accountNumber, userId } });
+    if (!account) throw new Error("Account not found or access denied");
+    return account;
+}
+
+async function changeAccountInfo(userId, accountNumber, updateData) {
+    const account = await AccountModel.findOne({ where: { accountNumber, userId } });
+    if (!account) throw new Error("Account not found or access denied");
+
+    const { accountName } = updateData;
+    if (accountName) account.accountName = accountName;
+    await account.save();
+    return await AccountModel.findOne({ where: { accountNumber, userId } });
+}
+
+async function deleteAccount(userId, accountNumber) {
+    const account = await AccountModel.findOne({ where: { accountNumber, userId } });
+    if (!account) throw new Error("Account not found or access denied");
+    await account.destroy();
+}
+
+export const accountService = { getAllUserAccounts, createNewAccount, getAccountInfo, changeAccountInfo, deleteAccount };
+export default accountService;
