018e528de9454e01d074985db130085d79fd5e07
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-03 10:53:29 +0200

    Add basic accountservices declarations

--- src/services/account.service.js
+++ src/services/account.service.js
@@ -1,4 +1,13 @@
+import sequelize from "../config/db.js";
 import { AccountModel } from "../models/index.js";
+import { log } from "../utils/utils.js";
+import { Op } from "sequelize";
 
-export const accountServices = {};
+async function getAllUserAccounts() {};
+async function createNewAccount() {};
+async function getAccountInfo() {};
+async function changeAccountInfo() {};
+async function deleteAccount() {};
+
+export const accountServices = {getAllUserAccounts, createNewAccount, getAccountInfo, changeAccountInfo, deleteAccount };
 export default accountServices;
