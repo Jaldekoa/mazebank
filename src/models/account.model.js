41a51fa8e386858d15f604ef653a9500e30b052b
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-03 22:08:39 +0200

    Add hooks to account.model.js

--- src/models/account.model.js
+++ src/models/account.model.js
@@ -21,13 +21,50 @@ const AccountModel = sequelize.define(
     accountNumber: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
     },
     balance: {
       type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
+      defaultValue: 0,
     },
   },
-  { tableName: "Accounts", timestamps: true },
+  {
+    tableName: "Accounts",
+    timestamps: true,
+    hooks: {
+      beforeValidate: async (account) => {
+        const lastAccount = await AccountModel.findOne({ order: [["id", "DESC"]], attributes: ["accountNumber"], raw: true });
+        let nextNumber = 1;
+        
+        if (lastAccount && lastAccount.accountNumber) {
+          const lastNumberStr = lastAccount.accountNumber.replace("ES", "");
+          nextNumber = parseInt(lastNumberStr, 10) + 1;
+        }
+        
+        account.accountNumber = `ES${nextNumber.toString().padStart(6, "0")}`;
+        account.balance = 0;
+      },
+      
+      beforeUpdate: (account) => {
+        if (account.changed('balance')) {
+          const previousBalance = account._previousDataValues.balance;
+          const currentBalance = account.getDataValue('balance');
+
+          if (Number(previousBalance) !== Number(currentBalance)) throw new Error("The balance can only be changed through transactions.");
+        }
+
+        if (account.changed('accountNumber')) account.accountNumber = account._previousDataValues.accountNumber;
+      },
+      
+      beforeDestroy: (account) => {
+        if (Number(account.balance) !== 0) {
+            throw new Error("Cannot delete an account with remaining balance.");
+        }
+      },
+
+    },
+  },
 );
 
 export default AccountModel;
