97b8ac8700fa110fdb387e5f0385fdc86f381b89
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-03 17:30:08 +0200

    Import and use rate limiter

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import { checkDB, syncDB } from "./config/db.js";
+import { rateLimit } from "express-rate-limit";
 import router from "./routes/routes.js";
 import { log } from "./utils/utils.js";
 import express from "express";
 import cors from "cors";
 
+const PORT = process.env.PORT || 3000;
+
 const corsOptions = {
-  origin: ["https://mazebank.com", "http://localhost:3000"],
+  origin: ["https://mazebank.com", `http://localhost:${PORT}`],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
-const PORT = process.env.PORT || 3000;
+const limiter = rateLimit({ windowMs: 5 * 60 * 1000, max: 100, standardHeaders: true, legacyHeaders: false });
+
 const app = express();
 
+app.use(limiter);
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
