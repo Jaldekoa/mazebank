0a80556ca16fcf520adb0e58ae8bf607e3b3a2bc
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-03 10:52:18 +0200

    Add basic declarations of controller functions

--- src/controllers/api/account.controller.js
+++ src/controllers/api/account.controller.js
@@ -1,4 +1,10 @@
 import accountServices from "../../services/account.service.js";
 
-export const accountController = {};
+async function getAllUserAccounts(req, res) {};
+async function createNewAccount(req, res) {};
+async function getAccountInfo(req, res) {};
+async function changeAccountInfo(req, res) {};
+async function deleteAccount(req, res) {};
+
+export const accountController = { getAllUserAccounts, createNewAccount, getAccountInfo, changeAccountInfo, deleteAccount };
 export default accountController;
